bca652ea233a70990442c0b327810934b7afed19
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-28 22:53:38 +0530

    variables usage

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Amarnath
+PERSON2=Vamshi
+
+echo "$PERSON1::Hi $PERSON2,how are you?"
+echo "$PERSON2::I'm fine..! what about you?"
+echo "$PERSON1::I'm doing good $PERSON2.what's going on?"
+echo "$PERSON2::I am learning shell script"
