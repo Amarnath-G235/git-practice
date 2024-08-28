d41538d4a2976573886b6bb5397a4691500fc031
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-28 22:45:41 +0530

    conversation between two persons

---
+++ 03-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Amarnath::Hi Vamshi,how are you?"
+echo "vamshi::I'm fine..! what about you?"
+echo "Amarnath::I'm doing good vamshi.what's going on?"
+echo "Vamshi::I am learning shell script"
