d263c78364b45dd9e0191e19e498046565d41855
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-29 12:09:20 +0530

    variables with arguments

---
+++ 06-variables-args.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+# we denote arguments/args with symbol $
+# when we denote varibles with arguments we need to pass the values
+# for those arguments
+# Above we wrote only two variables, so we need to pass two values only
+
+echo "$PERSON1::Hi $PERSON2,how are you?"
+echo "$PERSON2::I'm fine..! what about you?"
+echo "$PERSON1::I'm doing good $PERSON2.what's going on?"
+echo "$PERSON2::I am learning shell script"
