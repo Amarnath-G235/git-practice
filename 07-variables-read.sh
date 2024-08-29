2464aed496a1e748da977e58a4051588db0be9a6
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-29 12:23:08 +0530

    using read command

---
+++ 07-variables-read.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter your username:"
+
+# read command will take the input from user and
+# It will put that input into the variable 'USERNAME'
+
+read USERNAME
+
+echo "entered username is:$USERNAME"
