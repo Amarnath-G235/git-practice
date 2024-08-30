2eb57b093d186177e8997d095a3a92a0a4ed0483
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-30 11:01:38 +0530

    addition of two numbers

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -5,6 +5,9 @@ NUM=$1
 if [ $NUM -gt 20 ]
 then
    echo "given number $NUM is greater than 20"
+else if [ $NUM -eq 20 ]
+then
+   echo "given number $NUM is equal to 20"
 else
    echo "given number $NUM is less than 20"
 fi
