b6263aaddd88f0913583ef1f5feaad800c4ba83b
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-30 17:25:59 +0530

    function usage another type

---
+++ 17-function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# checking if the user has root access or not
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo "command faild..try again"
+       exit 1
+    else
+       echo "command failed"
+}
+
+if [ $USERID -ne 0 ]
+then 
+   echo "please run this script with sudo previleges"
+   exit 1
+fi
+
+#checking if mysql is already installed or not. if not installed going to install it
+
+dnf list installed mysql
+
+VALIDATE $?
