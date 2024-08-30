cfeea2e3ada95ec6a72846a84e66f5999d0cc205
Amarnathg <amarnath.gotru4@gmail.com>
2024-08-30 13:31:00 +0530

    mysql installation

---
+++ 15-installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# checking if the user has root access or not
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+   echo "please run this script with sudo previleges"
+   exit1
+fi
+
+#checking if mysql is already installed or not. if not installed going to install it
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+   echo "mysql is not installed, please instll it"
+   dnf install mysql -y
+else
+   echo "mysql is already installed,nothing to do"
+fi
