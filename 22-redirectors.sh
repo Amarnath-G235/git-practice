695dc9bc87905f677dbf42d5b64bbd4e90a0a2a4
Amarnathg <amarnath.gotru4@gmail.com>
2024-09-02 13:39:20 +0530

    LOOPS-REDIRECTORS-PACKAGE INSTALLATION

--- 22-redirectors.sh
+++ 22-redirectors.sh
@@ -13,14 +13,14 @@ N="\e[0m"
 Y="\e[33m"
 
 CHECK_ROOT(){
-if [ $USERID -ne 0 ]
-then 
-   echo -e " please run this script with sudo previleges" & >> $LOG_FILE
-   exit 1
-fi
+    if [ $USERID -ne 0 ]
+    then 
+       echo -e "$R please run this script with sudo previleges $N" & >> $LOG_FILE
+       exit 1
+    fi
 }
 
-VALIDATION(){
+VALIDATE(){
     if [ $1 -ne 0 ]
     then 
       echo -e " $2 is $R failed.. $N" & >>$LOG_FILE
@@ -31,13 +31,13 @@ VALIDATION(){
 }
 
 USAGE(){
-    echo -e " $R usage :: $N sudo $0 PACKAGE1 PACKAGE2 PACKAGE3.."
+    echo -e " $R usage :: $N sudo sh.22-redirectors.sh PACKAGE1 PACKAGE2 PACKAGE3.."
     exit 1
 }
 
 CHECK_ROOT
 
-if [ $# -e 0 ]
+if [ $# -eq 0 ]
 then
    USAGE
 fi
@@ -45,12 +45,12 @@ fi
 for PACKAGE in $@
 do
    dnf list installed $PACKAGE &>> $LOG_FILE
-   if [ $? -ne 0 ]
+    if [ $? -ne 0 ]
     then 
        echo -e "$PACKAGE is $R not installed.. going to install it $N" &>> $LOG_FILE
        dnf install $PACKAGE -y &>>$LOG_FILE
        VALIDATION $? "installing $PACKAGE"
     else
        echo -e "$PACKAGE $Y already installed. you cannot install twice.$N" >> $LOG_FILE
-fi
+    fi
 done
