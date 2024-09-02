1050170a8737c2b55a6b3e39c0c44ff1dfd27938
Amarnathg <amarnath.gotru4@gmail.com>
2024-09-02 13:30:21 +0530

    LOOPS-REDIRECTORS-PACKAGE INSTALLATION

--- 22-redirectors.sh
+++ 22-redirectors.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # $0--> will gives us script name.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
@@ -15,7 +15,7 @@ Y="\e[33m"
 CHECK_ROOT(){
 if [ $USERID -ne 0 ]
 then 
-   echo -e " please run this script with sudo previleges" &>>$LOG_FILE
+   echo -e " please run this script with sudo previleges" & >> $LOG_FILE
    exit 1
 fi
 }
